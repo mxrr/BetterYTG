de2a45468866d5ca909bc3a15f4c37cfb3b73f19
mxr <eetu.kivelae@gmail.com>
2021-01-03 21:24:51 +0200

    Fixed twitch emotes

--- src/content/Emotes/index.js
+++ src/content/Emotes/index.js
@@ -1,11 +1,10 @@
-import axios from 'axios';
-import PersistentSyncStorage from 'src/helpers/PersistentSyncStorage';
+import PersistentSyncStorage from "src/helpers/PersistentSyncStorage";
 
-import CustomEmotes from 'src/assets/emotes/dictionary.json';
-import OldIceEmotes from './oldIce.json';
+import CustomEmotes from "src/assets/emotes/dictionary.json";
+import OldIceEmotes from "./oldIce.json";
+import TwitchEmotes from "./twitch.json";
 
-import Emote from './Emote';
-
+import Emote from "./Emote";
 
 class Emotes {
   constructor() {
@@ -15,32 +14,32 @@ class Emotes {
 
   init() {
     return Promise.all([
-      (PersistentSyncStorage.data.options['enableTwitchEmotes'] && this.loadTwitch()),
-      (PersistentSyncStorage.data.options['enableBetterYTGEmotes'] && this.loadBetterYTG()),
-      (PersistentSyncStorage.data.options['enableOldIceEmotes'] && this.loadOldIce())
+      PersistentSyncStorage.data.options["enableTwitchEmotes"] &&
+        this.loadTwitch(),
+      PersistentSyncStorage.data.options["enableBetterYTGEmotes"] &&
+        this.loadBetterYTG(),
+      PersistentSyncStorage.data.options["enableOldIceEmotes"] &&
+        this.loadOldIce(),
     ]);
   }
 
   loadTwitch() {
-    axios
-      .get('https://twitchemotes.com/api_cache/v3/global.json')
-      .then(({ data }) => {
-        const emoteCodes = Object.keys(data);
+    const emoteCodes = Object.keys(TwitchEmotes);
 
-        for(let i = emoteCodes.length-1; i >= 0; i--) {
-          const emote = data[emoteCodes[i]];
-          const url = `https://static-cdn.jtvnw.net/emoticons/v1/${emote.id}/1.0`;
-          this.dictionary.set(emote.code, new Emote({ code: emote.code, url }));
-        }
-      });
+    for (let i = emoteCodes.length - 1; i >= 0; i--) {
+      const code = emoteCodes[i];
+      const emoteId = TwitchEmotes[code];
+      const url = `https://static-cdn.jtvnw.net/emoticons/v1/${emoteId}/1.0`;
+      this.dictionary.set(code, new Emote({ code, url }));
+    }
   }
 
   loadBetterYTG() {
     const emoteCodes = Object.keys(CustomEmotes);
 
-    for(let i = emoteCodes.length-1; i >= 0; i--) {
+    for (let i = emoteCodes.length - 1; i >= 0; i--) {
       const code = emoteCodes[i];
-      const filename = CustomEmotes[code]; 
+      const filename = CustomEmotes[code];
       const url = chrome.runtime.getURL(`/assets/emotes/images/${filename}`);
       this.dictionary.set(code, new Emote({ code, url }));
     }
@@ -49,7 +48,7 @@ class Emotes {
   loadOldIce() {
     const emoteCodes = Object.keys(OldIceEmotes);
 
-    for(let i = emoteCodes.length-1; i >= 0; i--) {
+    for (let i = emoteCodes.length - 1; i >= 0; i--) {
       const code = emoteCodes[i];
       const emoteId = OldIceEmotes[code];
       const url = `https://static-cdn.jtvnw.net/emoticons/v1/${emoteId}/1.0`;
@@ -70,4 +69,4 @@ class Emotes {
   }
 }
 
-export default new Emotes;
+export default new Emotes();
