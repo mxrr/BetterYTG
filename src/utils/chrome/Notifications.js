45b72f4861dc7d92c9bc61d4c6041a63ed3f37bb
mxr <eetu.kivelae@gmail.com>
2021-01-03 20:10:02 +0200

    Fixes for firefox

--- src/utils/chrome/Notifications.js
+++ src/utils/chrome/Notifications.js
@@ -1,83 +1,87 @@
 class Notifications {
-
   create(notificationId = null, options) {
     // notificationId is optional
-    if(typeof notificationId === 'object') {
+    if (typeof notificationId === "object") {
       options = notificationId;
       notificationId = null;
     }
 
     return new Promise((res, rej) => {
-      // resolve args = notificationId:string 
-      chrome.notifications.create(notificationId, options, res);
+      // resolve args = notificationId:string
+      if (chrome.notifications)
+        chrome.notifications.create(notificationId, options, res);
+      else return browser.notifications.create(notificationId, options);
     });
   }
 
   update(notificationId, options) {
     return new Promise((res, rej) => {
       // resolve args = wasUpdated:boolean
-      chrome.notifications.update(notificationId, options, res);
+      if (chrome.notifications)
+        chrome.notifications.update(notificationId, options, res);
+      else return browser.notifications.update(notificationId, options);
     });
   }
 
   clear(notificationId) {
     return new Promise((res, rej) => {
       // resolve args = wasCleared:boolean
-      chrome.notifications.clear(notificationId, res);
+      if (chrome.notifications) chrome.notifications.clear(notificationId, res);
+      else return chrome.notifications.clear(notificationId);
     });
   }
 
   getAll() {
     return new Promise((res, rej) => {
       // resolve args = notifications:object
-      chrome.notifications.getAll(res);
+      if (chrome.notifications) chrome.notifications.getAll(res);
+      else return browser.notifications.getAll(res);
     });
   }
 
   getPermissionLevel() {
     return new Promise((res, rej) => {
       // resolve args = level:PermissionLevel (https://developer.chrome.com/apps/notifications#type-PermissionLevel)
-      chrome.notifications.getPermissionLevel(res);
+      if (chrome.notifications) chrome.notifications.getPermissionLevel(res);
     });
   }
 
   listen(event, notificationId = null, callback) {
     // event = 'onClosed' | 'onClicked' | 'onButtonClicked' | 'onPermissionLevelChanged' | 'onShowSettings'
     // notificationId is optional
-    if(typeof notificationId === 'function') {
+    if (typeof notificationId === "function") {
       callback = notificationId;
       notificationId = null;
     }
 
-    if(event === 'onPermissionLevelChanged' || event === 'onShowSettings') {
+    if (event === "onPermissionLevelChanged" || event === "onShowSettings") {
       return this._nonNotificationIdListen(event, callback);
     }
 
     /**
      * https://developer.chrome.com/apps/notifications#events
-     * 
+     *
      * Resolve args (by event):
      * onClosed = notificationId:string, byUser:boolean
      * onClicked = notificationId:string
      * onButtonClicked = notificationId:string, buttonIndex:integer
-     * 
+     *
      * onPermissionLevelChanged = level:PermissionLevel (https://developer.chrome.com/apps/notifications#type-PermissionLevel)
      * onShowSettings = (none)
      */
 
     // This callback relates only to those events that have notificationId arg
     const ListenerCallback = (() => {
-      if(notificationId !== null) {
+      if (notificationId !== null) {
         return (passedNotificationId, ...args) => {
-          if(notificationId === passedNotificationId) {
+          if (notificationId === passedNotificationId) {
             callback(passedNotificationId, ...args);
           }
-        }
+        };
       } else {
         return callback;
       }
     })();
-
 
     chrome.notifications[event].addListener(ListenerCallback);
   }
